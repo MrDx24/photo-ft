ba657917f915ed7a69b58c7c68e4b15d1c14f139
Deep Patadiya <deeppatadiya24@gmail.com>
2023-08-01 00:46:15 +0530

    first commit

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,13 +1,3 @@
 export const environment = {
-  production : false,
-  firebaseConfig : {
-    apiKey: "AIzaSyAWxupW9pLmB296dSmvZFSRwxQXSV6-d-c",
-    authDomain: "poc-photography.firebaseapp.com",
-    databaseURL: "https://poc-photography-default-rtdb.firebaseio.com",
-    projectId: "poc-photography",
-    storageBucket: "poc-photography.appspot.com",
-    messagingSenderId: "452853173703",
-    appId: "1:452853173703:web:04f55f230619432d429de2",
-    measurementId: "G-GBTR0MWRYL"
-  }
+  production : false
 };
